5e7e2a9d7467ab180f11a79a2b23048517e65f28
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2024-01-05 17:28:08 +0200

    initial commit

---
+++ documentation/credits.md
@@ -0,0 +1,1 @@
+<a href="https://www.flaticon.com/free-icons/wind" title="wind icons">Wind icons created by Freepik - Flaticon</a>
---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Flow Web</h1>
+      </header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;

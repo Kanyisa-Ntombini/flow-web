62584c55fd34d2227c7fa4a93b0159fe6c7127c5
Kanyisa Ntombini <71196826+Kanyisa-Ntombini@users.noreply.github.com>
2024-03-22 01:08:53 +0200

    Completed bar bones app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,13 @@
 import './App.css';
-// import Header from '../Header/Header.js';
-// import Main from '../Main/Main.js';
+import Header from '../Header/Header.js';
+import Main from '../Main/Main.js';
 import Footer from '../Footer/Footer.js';
-/*
-1. Header
-  - title
-  - Navigation Bar?
-
-2. Main
-- Search Bar
-  - display name of user
-  - display all users photos
-  - each photo must have a download button
-
-3. Footer with my name
-*/
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
-      {/* <Main /> */}
+      <Header />
+      <Main />
       <Footer />
     </div>
   );

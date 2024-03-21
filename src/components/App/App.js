a586eb32de6db620c1cd301ad4265b8dadb4bb75
Kanyisa Ntombini <71196826+Kanyisa-Ntombini@users.noreply.github.com>
2024-03-22 01:04:48 +0200

    Footer component is created and applied to app component

---
+++ src/components/App/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+// import Header from '../Header/Header.js';
+// import Main from '../Main/Main.js';
+import Footer from '../Footer/Footer.js';
+/*
+1. Header
+  - title
+  - Navigation Bar?
+
+2. Main
+- Search Bar
+  - display name of user
+  - display all users photos
+  - each photo must have a download button
+
+3. Footer with my name
+*/
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      {/* <Main /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 K Ntombini</p>
+    </footer>
+  );
+}
+
+export default Footer;

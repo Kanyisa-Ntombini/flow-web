73c668feb57cd0e74333e687a0e8d22802a565a2
Kanyisa Ntombini <71196826+Kanyisa-Ntombini@users.noreply.github.com>
2024-03-22 02:15:43 +0200

    Added code to get access token from instagram

---
+++ src/instagram_api/utility_functions.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const getAuthorizationCode = (authorizationWindowUrl) => {
+  return authorizationWindowUrl.split('code=')[1].split('#_')[0];
+};
+
+const getAccessToken = async (authorizationWindowUrl) => {
+  const code = getAuthorizationCode(authorizationWindowUrl);
+  const response = await axios({
+    url: 'https://api.instagram.com/oauth/access_token',
+    method: 'post',
+    headers: {
+      'Content-type': 'multipart/form-data',
+    },
+    data: {
+      client_id: '1409820282971631',
+      client_secret: '7f291034019d3dbea408e8635ba300e4',
+      grant_type: 'authorization_code',
+      redirect_uri: 'https://flow-web-888.netlify.app/',
+      code: code,
+    },
+  });
+  console.log(response);
+  return response.data;
+  /* 
+   {
+   access_token: 'IGQWRPSkF6D',
+   user_id: 754745747
+ }
+   */
+};
+
+export { getAccessToken };
